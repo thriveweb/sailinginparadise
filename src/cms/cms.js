e6f99ca07e363023f71a663885764a9c5c033cdd
Danny Barinshtein <danny@thriveweb.com.au>
2019-07-08 23:37:27 +1000

    latest

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,3 +1,8 @@
+import CMS from 'netlify-cms-app'
+import uploadcare from 'netlify-cms-media-library-uploadcare';
+
+CMS.registerMediaLibrary(uploadcare);
+
 // import React from 'react'
 // import CMS from 'netlify-cms-app'
 // import './cms-utils'
