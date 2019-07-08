aa9ad988fe9263c651363232ba5cd41e0ddefc3e
Danny Barinshtein <danny@thriveweb.com.au>
2019-07-08 23:33:40 +1000

    latest

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -17,44 +17,43 @@ import { SingleCaseStudyTemplate } from '../templates/SingleCaseStudy'
 import { SingleBoatTourTemplate } from '../templates/SingleBoatTour'
 
 CMS.init()
-CMS.registerPreviewStyle('/styles.css')
+// CMS.registerPreviewStyle('/styles.css')
 
-CMS.registerPreviewTemplate('home-page', ({ entry }) => (
-  <HomePageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('about-page', ({ entry }) => (
-  <AboutPageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('contact-page', ({ entry }) => (
-  <ContactPageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('infoPages', ({ entry }) => (
-  <DefaultPageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('blog-page', ({ entry }) => (
-  <BlogIndexTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('posts', ({ entry }) => (
-  <SinglePostTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('privateCharters', ({ entry }) => (
-  <PrivateChartersTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('booking-page', ({ entry }) => (
-  <BookingPageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('cruises', ({ entry }) => (
-  <CruisesTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('case-studies-page', ({ entry }) => (
-  <CaseStudiesTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('ourBoats', ({ entry }) => (
-  <BoatsPageTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('caseStudy', ({ entry }) => (
-  <SingleCaseStudyTemplate {...entry.toJS().data} />
-))
-CMS.registerPreviewTemplate('boatTours', ({ entry }) => (
-  <SingleBoatTourTemplate {...entry.toJS().data} />
-))
+CMS.registerPreviewTemplate('home-page', <HomePageTemplate />)
+
+// CMS.registerPreviewTemplate('about-page', ({ entry }) => (
+//   <AboutPageTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('contact-page', ({ entry }) => (
+//   <ContactPageTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('infoPages', ({ entry }) => (
+//   <DefaultPageTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('blog-page', ({ entry }) => (
+//   <BlogIndexTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('posts', ({ entry }) => (
+//   <SinglePostTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('privateCharters', ({ entry }) => (
+//   <PrivateChartersTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('booking-page', ({ entry }) => (
+//   <BookingPageTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('cruises', ({ entry }) => (
+//   <CruisesTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('case-studies-page', ({ entry }) => (
+//   <CaseStudiesTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('ourBoats', ({ entry }) => (
+//   <BoatsPageTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('caseStudy', ({ entry }) => (
+//   <SingleCaseStudyTemplate {...entry.toJS().data} />
+// ))
+// CMS.registerPreviewTemplate('boatTours', ({ entry }) => (
+//   <SingleBoatTourTemplate {...entry.toJS().data} />
+// ))
