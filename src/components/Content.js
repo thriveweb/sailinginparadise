9a39b84781abce50863db4e48d588c1ef2a9dec2
Amber Gowans <amber@thriveweb.com.au>
2019-07-10 22:44:18 +0200

    markdown

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,66 @@
 import React from 'react'
+import Marked from 'react-markdown'
 import PropTypes from 'prop-types'
 
-export const HTMLContent = ({ content, className }) => (
-  <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
-)
+import './Content.css'
 
-const Content = ({ content, className }) => (
-  <div className={className}>{content}</div>
-)
-
-Content.propTypes = {
-  content: PropTypes.node,
-  className: PropTypes.string,
+const encodeMarkdownURIs = (source = '') => {
+  const markdownLinkRegex = /\[(?:\[[^\]]*\]|[^[\]])*\]\([ \t]*<?((?:\([^)]*\)|[^()])*?)>?[ \t]*(['"].*?\6[ \t]*)?\)/g
+  return source.replace(markdownLinkRegex, (match, linkURI) => {
+    if (!linkURI) return match
+    const replaced = match.replace(linkURI, encodeURI(linkURI))
+    return replaced
+  })
 }
 
-HTMLContent.propTypes = Content.propTypes
+const Image = ({ nodeKey, src, alt, ...props }) => {
+  const decodedSrc = decodeURI(src)
+  return (
+    <img className="Content--Image" {...props} src={decodedSrc} alt={alt} />
+  )
+}
+
+const HtmlBlock = ({ value }) => {
+  if (value.indexOf('<iframe') !== 0) return value
+  return (
+    <div
+      className={`Content--Iframe`}
+      dangerouslySetInnerHTML={{
+        __html: value
+      }}
+    />
+  )
+}
+
+const Content = ({ source, src, className = '' }) => {
+  // accepts either html or markdown
+  source = source || src || ''
+  if (source.match(/^</)) {
+    // If source is html (starts with '<')
+    return (
+      <div
+        className={`Content ${className}`}
+        dangerouslySetInnerHTML={{ __html: source }}
+      />
+    )
+  }
+
+  return (
+    <Marked
+      className={`Content ${className}`}
+      source={encodeMarkdownURIs(source)}
+      renderers={{
+        image: Image,
+        html: HtmlBlock
+      }}
+    />
+  )
+}
+
+Content.propTypes = {
+  source: PropTypes.string,
+  src: PropTypes.string,
+  className: PropTypes.string
+}
 
 export default Content
