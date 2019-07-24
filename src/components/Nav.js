46b478e619d6145ec9247c2094feb5c94f7e1402
Alex <alex@thriveweb.com.au>
2019-07-24 16:48:59 +1000

    mobile nav fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,7 +14,13 @@ export default class Nav extends Component {
     popupActive: false
   }
 
-  handleMenuToggle = () => this.setState({ active: !this.state.active })
+  handleMenuToggle = () => {
+    this.setState({ active: !this.state.active })
+    document.body.style.overflow = !this.state.popupActive ? 'hidden' : 'auto'
+    document.documentElement.style.overflow = !this.state.popupActive
+      ? 'hidden'
+      : 'auto'
+  }
   // Only close nav if it is open
   handleLinkClick = () => this.state.active && this.handleMenuToggle()
 
@@ -24,7 +30,9 @@ export default class Nav extends Component {
     })
 
     document.body.style.overflow = !this.state.popupActive ? 'hidden' : 'auto'
-    document.documentElement.style.overflow = !this.state.popupActive ? 'hidden' : 'auto'
+    document.documentElement.style.overflow = !this.state.popupActive
+      ? 'hidden'
+      : 'auto'
   }
 
   render() {
@@ -33,11 +41,17 @@ export default class Nav extends Component {
         query={graphql`
           query NavQuery {
             allMarkdownRemark(
-              filter: { fields: { contentType: { in: ["pages", "defaultPages", "boatTours", "posts"] }}}
+              filter: {
+                fields: {
+                  contentType: {
+                    in: ["pages", "defaultPages", "boatTours", "posts"]
+                  }
+                }
+              }
             ) {
               edges {
                 node {
-                  frontmatter{
+                  frontmatter {
                     title
                   }
                   fields {
@@ -48,102 +62,147 @@ export default class Nav extends Component {
             }
           }
         `}
-
         render={data => {
           const { allMarkdownRemark } = data
           const { navList } = this.props
           const { active, popupActive, menuItemActive } = this.state
 
-          const navItems = (navList && _get(navList, 'frontmatter.navItems')) || []
+          const navItems =
+            (navList && _get(navList, 'frontmatter.navItems')) || []
           const bookingPopup = _get(this.props, 'bookingPopup') || []
           const popup = _get(bookingPopup.frontmatter, 'bookingPopup') || []
 
-          return <Fragment>
-            <nav className={`Nav ${active ? 'Nav-active' : ''}`}>
-              <div className="Nav--Container container large">
-                <Link to="/" onClick={this.handleLinkClick}>
-                  <Logo />
-                </Link>
-                <div className="Nav--Links">
-                  {navItems.map(({ title, subNavItems }, index) => {
-                    const foundItem = allMarkdownRemark.edges.find(({ node }) => _get(node, 'frontmatter.title') === title)
-                    const foundItemSlug = _get(foundItem, 'node.fields.slug')
+          return (
+            <Fragment>
+              <nav className={`Nav ${active ? 'Nav-active' : ''}`}>
+                <div className="Nav--Container container large">
+                  <Link to="/" onClick={this.handleLinkClick}>
+                    <Logo />
+                  </Link>
+                  <div className="Nav--Links">
+                    {navItems.map(({ title, subNavItems }, index) => {
+                      const foundItem = allMarkdownRemark.edges.find(
+                        ({ node }) => _get(node, 'frontmatter.title') === title
+                      )
+                      const foundItemSlug = _get(foundItem, 'node.fields.slug')
 
-                    if(!foundItemSlug)
+                      if (!foundItemSlug)
+                        return (
+                          <li
+                            key={`nav-${index}`}
+                            className={`NavLink ${
+                              subNavItems ? 'hasChildren' : ''
+                            } ${
+                              foundItemSlug === '/private-charters/'
+                                ? 'two-column'
+                                : ''
+                            } ${menuItemActive === index ? 'active' : ''}`}
+                            onClick={() =>
+                              this.setState({
+                                menuItemActive:
+                                  menuItemActive === index ? false : index
+                              })
+                            }
+                          >
+                            <span>{title}</span>
+                            {subNavItems && (
+                              <ul className="subMenu">
+                                {subNavItems.map(({ title }, index) => {
+                                  const foundItem = allMarkdownRemark.edges.find(
+                                    ({ node }) =>
+                                      _get(node, 'frontmatter.title') === title
+                                  )
+                                  const foundItemSlug = _get(
+                                    foundItem,
+                                    'node.fields.slug'
+                                  )
+
+                                  return (
+                                    foundItemSlug && (
+                                      <li
+                                        key={`subNav-${index}`}
+                                        className="NavLink"
+                                      >
+                                        <Link to={foundItemSlug}>{title}</Link>
+                                      </li>
+                                    )
+                                  )
+                                })}
+                              </ul>
+                            )}
+                          </li>
+                        )
+
                       return (
                         <li
                           key={`nav-${index}`}
-                          className={`NavLink ${subNavItems ? 'hasChildren' : ''} ${
-                            foundItemSlug === '/private-charters/' ? 'two-column' : ''
+                          className={`NavLink ${
+                            subNavItems ? 'hasChildren' : ''
+                          } ${
+                            foundItemSlug === '/private-charters/'
+                              ? 'two-column'
+                              : ''
                           } ${menuItemActive === index ? 'active' : ''}`}
-                          onClick={() => this.setState({ menuItemActive: menuItemActive === index ? false : index })}
                         >
-                          <span>{title}</span>
+                          <Link to={foundItemSlug}>{title}</Link>
+                          <p
+                            className="toggle-subNav"
+                            onClick={() =>
+                              this.setState({
+                                menuItemActive:
+                                  menuItemActive === index ? false : index
+                              })
+                            }
+                          >
+                            +
+                          </p>
                           {subNavItems && (
                             <ul className="subMenu">
                               {subNavItems.map(({ title }, index) => {
-                                const foundItem = allMarkdownRemark.edges.find(({ node }) => _get(node, 'frontmatter.title') === title)
-                                const foundItemSlug = _get(foundItem, 'node.fields.slug')
+                                const foundItem = allMarkdownRemark.edges.find(
+                                  ({ node }) =>
+                                    _get(node, 'frontmatter.title') === title
+                                )
+                                const foundItemSlug = _get(
+                                  foundItem,
+                                  'node.fields.slug'
+                                )
 
-                                return foundItemSlug &&
-                                  <li key={`subNav-${index}`} className="NavLink">
-                                    <Link to={foundItemSlug}>{title}</Link>
-                                  </li>
+                                return (
+                                  foundItemSlug && (
+                                    <li
+                                      key={`subNav-${index}`}
+                                      className="NavLink"
+                                    >
+                                      <Link to={foundItemSlug}>{title}</Link>
+                                    </li>
+                                  )
+                                )
                               })}
                             </ul>
                           )}
                         </li>
                       )
-
-                    return (
-                      <li
-                        key={`nav-${index}`}
-                        className={`NavLink ${subNavItems ? 'hasChildren' : ''} ${
-                          foundItemSlug === '/private-charters/' ? 'two-column' : ''
-                        } ${menuItemActive === index ? 'active' : ''}`}
-                      >
-                        <Link to={foundItemSlug}>{title}</Link>
-                        <p
-                          className='toggle-subNav'
-                          onClick={() => this.setState({ menuItemActive: menuItemActive === index ? false : index })}
-                        >
-                          +
-                        </p>
-                        {subNavItems && (
-                          <ul className="subMenu">
-                            {subNavItems.map(({ title }, index) => {
-                              const foundItem = allMarkdownRemark.edges.find(({ node }) => _get(node, 'frontmatter.title') === title)
-                              const foundItemSlug = _get(foundItem, 'node.fields.slug')
-
-                              return foundItemSlug &&
-                                <li key={`subNav-${index}`} className="NavLink">
-                                  <Link to={foundItemSlug}>{title}</Link>
-                                </li>
-
-                            })}
-                          </ul>
-                        )}
-                      </li>
-                    )
-                  })}
+                    })}
+                  </div>
+                  <p className="nav-button" onClick={this.handlePopup}>
+                    Booking Enquiry
+                  </p>
+                  <button
+                    className="Button-blank Nav--MenuButton"
+                    onClick={this.handleMenuToggle}
+                  >
+                    {active ? <X /> : <Menu />}
+                  </button>
                 </div>
-                <p className="nav-button" onClick={this.handlePopup}>
-                  Booking Enquiry
-                </p>
-                <button
-                  className="Button-blank Nav--MenuButton"
-                  onClick={this.handleMenuToggle}
-                >
-                  {active ? <X /> : <Menu />}
-                </button>
-              </div>
-            </nav>
-            <BookingPopup
-              {...popup}
-              classActive={popupActive ? 'active' : ''}
-              handlePopup={this.handlePopup}
-            />
-          </Fragment>
+              </nav>
+              <BookingPopup
+                {...popup}
+                classActive={popupActive ? 'active' : ''}
+                handlePopup={this.handlePopup}
+              />
+            </Fragment>
+          )
         }}
       />
     )
