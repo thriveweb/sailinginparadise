fc4a157903f2e8e580127d938d5668d453d7b390
Alex <alex@thriveweb.com.au>
2019-07-24 18:09:28 +1000

    Fix missing links on featureed posts

--- src/components/FeaturedPosts.js
+++ src/components/FeaturedPosts.js
@@ -6,19 +6,26 @@ import PostCard from './PostCard'
 import './FeaturedPosts.css'
 
 export default ({ latestNews, posts }) => {
+  let postContent = []
+  postContent = posts
+    ? posts.edges.map(edge => ({
+        ...edge.node.frontmatter,
+        ...edge.node.fields
+      }))
+    : []
 
-	posts = posts ? posts.edges.map(edge => ({ ...edge.node.frontmatter })) : []
+  if (!postContent) return null
 
-	if(!posts) return null
-
-	return <section className='featuredPostsSection'>
-		<div className='container large'>
-			{latestNews && <IntroText content={latestNews} center />}
-			<div className='featuredPosts'>
-				{posts.map(( post, index ) =>
-					<PostCard {...post} featuredHome key={index} />
-				)}
-			</div>
-		</div>
-	</section>
+  return (
+    <section className="featuredPostsSection">
+      <div className="container large">
+        {latestNews && <IntroText content={latestNews} center />}
+        <div className="featuredPosts">
+          {postContent.map((post, index) => (
+            <PostCard {...post} featuredHome key={index} />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -19,46 +19,54 @@ const PostCard = ({
   excerpt,
   featuredHome,
   fields,
-  ...props,
-
+  ...props
 }) => {
-
-  if(featuredHome) className += ' featuredHome'
+  if (featuredHome) className += ' featuredHome'
 
   const contentType = _get(fields, 'contentType') || ''
 
-  return <Link to={slug} className={`PostCard ${className}`}>
+  return (
+    <Link to={slug} className={`PostCard ${className}`}>
       {featuredImage && (
         <div className="PostCard--Image relative">
-          {featuredImage && <Image background src={`${featuredImage}-/format/auto/-/quality/lighter/-/progressive/yes/-/resize/700/`} />}
+          {featuredImage && (
+            <Image
+              background
+              src={`${featuredImage}-/format/auto/-/quality/lighter/-/progressive/yes/-/resize/700/`}
+            />
+          )}
         </div>
       )}
       <div className="PostCard--Content">
         <div className="SinglePost--Meta">
-          {contentType === 'happySailors' && date
-          ? <time
+          {contentType === 'happySailors' && date ? (
+            <time
               className="SinglePost--Meta--Date"
               itemProp="dateCreated pubdate datePublished"
               date={date}
             >
               {_format(date, 'MMM YYYY')}
             </time>
-          : contentType !== 'happySailors' && date
-            ? <time
-                className="SinglePost--Meta--Date"
-                itemProp="dateCreated pubdate datePublished"
-                date={date}
-              >
-                {_format(date, 'D.MM.YYYY')}
-              </time>
-            : ''
-          }
+          ) : contentType !== 'happySailors' && date ? (
+            <time
+              className="SinglePost--Meta--Date"
+              itemProp="dateCreated pubdate datePublished"
+              date={date}
+            >
+              {_format(date, 'D.MM.YYYY')}
+            </time>
+          ) : (
+            ''
+          )}
           {!!categories.length && (
             <Fragment>
               <span>|</span>
-              <div className='meta-categories'>
+              <div className="meta-categories">
                 {categories.map((cat, index) => (
-                  <span key={cat.category} className="SinglePost--Meta--Category">
+                  <span
+                    key={cat.category}
+                    className="SinglePost--Meta--Category"
+                  >
                     {cat.category}
                     {/* Add a comma on all but last category */}
                     {index !== categories.length - 1 ? ',' : ''}
@@ -68,17 +76,26 @@ const PostCard = ({
             </Fragment>
           )}
         </div>
-        {contentType === 'happySailors' && <p className="PostCard--Subtitle">{title}</p>}
-        {icon && <Image src={icon} alt='' />}
-        {cruiseType ? <h3 className="PostCard--Title">{cruiseType}</h3> : <h3 className="PostCard--Title">{title}</h3>}
-        {excerpt &&
+        {contentType === 'happySailors' && (
+          <p className="PostCard--Subtitle">{title}</p>
+        )}
+        {icon && <Image src={icon} alt="" />}
+        {cruiseType ? (
+          <h3 className="PostCard--Title">{cruiseType}</h3>
+        ) : (
+          <h3 className="PostCard--Title">{title}</h3>
+        )}
+        {excerpt && (
           <Fragment>
-            <p className='excerpt'>{excerpt.slice(0, 70) + '...'}</p>
-            <p className='button'>Read More <ICONButtonArrows /></p>
+            <p className="excerpt">{excerpt.slice(0, 70) + '...'}</p>
+            <p className="button">
+              Read More <ICONButtonArrows />
+            </p>
           </Fragment>
-        }
+        )}
       </div>
     </Link>
+  )
 }
 
 export default PostCard
