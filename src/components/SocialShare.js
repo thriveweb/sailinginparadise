e2d5e1e744fdf4c8bf3ba4c55a1cba00d6c22ded
Alex <alex@thriveweb.com.au>
2019-07-25 09:59:37 +1000

    fix broken social share buttons

--- src/components/SocialShare.js
+++ src/components/SocialShare.js
@@ -3,18 +3,28 @@ import React from 'react'
 import { ICONFacebook, ICONTwitter } from './Icons'
 import './SocialShare.css'
 
-export default () =>
-
-	<ul className="share-buttons">
-		<p>SHARE ON</p>
-		<li>
-			<a href="https://www.facebook.com/sharer/sharer.php?u=&quote=" target="_blank" rel="noopener noreferrer" title="Share on Facebook">
-				<ICONFacebook />
-			</a>
-		</li>
-		<li>
-			<a href="https://twitter.com/intent/tweet?source=&text=:%20" target="_blank" rel="noopener noreferrer" title="Tweet">
-				<ICONTwitter />
-			</a>
-		</li>
-	</ul>
+export default () => (
+  <ul className="share-buttons">
+    <p>SHARE ON</p>
+    <li>
+      <a
+        href={`https://www.facebook.com/sharer/sharer.php?u=${document.location.href}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Share on Facebook"
+      >
+        <ICONFacebook />
+      </a>
+    </li>
+    <li>
+      <a
+        href={`https://twitter.com/intent/tweet?source=${document.location.href}&text=Sailing In Paradise - ${document.location.href}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Tweet"
+      >
+        <ICONTwitter />
+      </a>
+    </li>
+  </ul>
+)
