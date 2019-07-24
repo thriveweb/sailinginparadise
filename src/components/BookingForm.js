30897393f97a1b9cb1a48dbf5f32f8021731d2b9
Alex <alex@thriveweb.com.au>
2019-07-24 17:46:23 +1000

    booking form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -3,13 +3,13 @@ import { stringify, parse } from 'qs'
 import { serialize } from 'dom-form-serializer'
 import Select from './Select'
 import { ICONButtonArrows } from './Icons'
-import NumericInput from 'react-numeric-input';
+import NumericInput from 'react-numeric-input'
 import _get from 'lodash/get'
 import _startCase from 'lodash/startCase'
 import _format from 'date-fns/format'
 
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 import './BookingForm.css'
 
@@ -18,20 +18,22 @@ class Form extends React.Component {
     name: 'Booking Enquiry',
     action: '',
     honeypot: 'confirm',
-    successMessage: 'Thank you for enquiring about cruise with sailing in paradise, we will be in touch very soon',
-    errorMessage: 'There is a problem, your message has not been sent, please try contacting us via email'
+    successMessage:
+      'Thank you for enquiring about cruise with sailing in paradise, we will be in touch very soon',
+    errorMessage:
+      'There is a problem, your message has not been sent, please try contacting us via email'
   }
 
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       startDate: '',
       alert: '',
       disabled: false,
       labelDisplay: false
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
   handleSubmit = e => {
@@ -46,27 +48,27 @@ class Form extends React.Component {
     fetch(form.action + '?' + stringify(data), {
       method: 'POST'
     })
-    .then(res => {
-      if (res.ok) {
-        return res
-      } else {
-        throw new Error('Network error')
-      }
-    })
-    .then(() => {
-      form.reset()
-      this.setState({
-        alert: this.props.successMessage,
-        disabled: false
+      .then(res => {
+        if (res.ok) {
+          return res
+        } else {
+          throw new Error('Network error')
+        }
       })
-    })
-    .catch(err => {
-      console.error(err)
-      this.setState({
-        disabled: false,
-        alert: this.props.errorMessage
+      .then(() => {
+        form.reset()
+        this.setState({
+          alert: this.props.successMessage,
+          disabled: false
+        })
       })
-    })
+      .catch(err => {
+        console.error(err)
+        this.setState({
+          disabled: false,
+          alert: this.props.errorMessage
+        })
+      })
   }
 
   handleValueChange = e => {
@@ -78,7 +80,7 @@ class Form extends React.Component {
   handleChange(date) {
     this.setState({
       startDate: date
-    });
+    })
   }
 
   handleClick() {
@@ -97,7 +99,11 @@ class Form extends React.Component {
     const query = search ? parse(search.replace('?', '')) : ''
     const formCharter = query.charter
 
-    const formatting = <time itemProp="dateCreated pubdate datePublished" date={startDate}>{_format(startDate, 'MMM DD YYYY')}</time>
+    const formatting = (
+      <time itemProp="dateCreated pubdate datePublished" date={startDate}>
+        {_format(startDate, 'MMM DD YYYY')}
+      </time>
+    )
     const date = _get(formatting, 'props.children') || ''
     const charterType = _get(this, 'state.charterType')
 
@@ -156,61 +162,58 @@ class Form extends React.Component {
             </label>
             <label className="Form--Label full-width">
               <span>Approx. number of guests*</span>
-              <NumericInput
-                className="form-control"
-                min={0}
-                mobile
-                required
-              />
+              <NumericInput className="form-control" min={0} mobile required />
             </label>
             <Select
-              placeholder='Charter Type*'
-              name='charterType'
+              placeholder="Charter Type*"
+              name="charterType"
               selected={formCharter}
               handleValueChange={this.handleValueChange}
               options={[
-                "Raft Ups",
-                "Hens Parties",
-                "Birthday Parties",
-                "Corporate Events",
-                "Christmas Parties",
-                "Memorial Ashes Scattering",
-                "Wedding Proposals",
-                "Romantic Couples Only",
-                "Family with Kids",
-                "Holiday",
-                "Private Charters",
-                "Bucks Parties"
+                'Raft Up',
+                'Hens Party',
+                'Birthday Party',
+                'Corporate Event',
+                'Christmas Party',
+                'Memorial Ashes Scattering',
+                'Wedding Proposal',
+                'Romantic Couples Only',
+                'Family with Kids',
+                'Holiday',
+                'Private Charter',
+                'Bucks Party'
               ]}
             />
-            <div
-              className="Form--Label date-section"
-            >
+            <div className="Form--Label date-section">
               <DatePicker
-                name='date'
+                name="date"
                 required
-                className='Form--Input'
+                className="Form--Input"
                 selected={this.state.startDate}
                 onChange={this.handleChange}
-                autoComplete='off'
+                autoComplete="off"
               />
-              <p className={`date-label ${this.state.startDate ? 'active' : ''}`}>
+              <p
+                className={`date-label ${this.state.startDate ? 'active' : ''}`}
+              >
                 Preferred Date*
-                <span>(if unsure please select any date within Preffered month)</span>
+                <span>
+                  (if unsure please select any date within Preffered month)
+                </span>
               </p>
             </div>
             <Select
-              placeholder='How did you hear about us?*'
-              name='source'
+              placeholder="How did you hear about us?*"
+              name="source"
               options={[
-                "Web Search",
-                "Facebook",
-                "Instagram",
-                "Friend or family referral",
-                "Signage",
-                "Flyer",
-                "Sailed with us before",
-                "Other"
+                'Web Search',
+                'Facebook',
+                'Instagram',
+                'Friend or family referral',
+                'Signage',
+                'Flyer',
+                'Sailed with us before',
+                'Other'
               ]}
             />
             <input
@@ -219,9 +222,13 @@ class Form extends React.Component {
               className="Form--Input-honey"
               placeholder="Leave blank if you are a human"
             />
-            <input type="hidden" name="subject" value={`${_startCase(charterType)} - ${date}`} />
+            <input
+              type="hidden"
+              name="subject"
+              value={`${_startCase(charterType)} - ${date}`}
+            />
             <input type="hidden" name="form-name" value={name} />
-            <div className='form-footer'>
+            <div className="form-footer">
               <div>
                 <input
                   className="button Form--SubmitButton"
